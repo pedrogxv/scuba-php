a2d7e6bb319e8b622fcb3556d5751cd0daa00e9f
Pedro Gabriel <63373521+PedroGXV@users.noreply.github.com>
2023-04-27 13:55:06 -0300

    Adicionado classe para e-mails: Mail

---
+++ Mail.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App;
+
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+
+class Mail
+{
+    private readonly PHPMailer $PHPMailer;
+
+    public function __construct(
+        private readonly string $to,
+        private readonly string $subject,
+        private readonly string $htmlPath,
+    )
+    {
+    }
+
+    private function createMailerInstance(): void
+    {
+        $this->PHPMailer = new PHPMailer();
+        $this->PHPMailer->isSMTP();
+        $this->PHPMailer->Host = $_ENV["MAIL_HOST"];
+        $this->PHPMailer->SMTPAuth = true;
+        $this->PHPMailer->Port = $_ENV["MAIL_PORT"];
+        $this->PHPMailer->Username = $_ENV["MAIL_USERNAME"];
+        $this->PHPMailer->Password = $_ENV["MAIL_PASSWORD"];
+    }
+
+    /**
+     * @throws Exception
+     */
+    private function mountMail(): void
+    {
+        $this->PHPMailer->Subject = $this->subject;
+        $this->PHPMailer->addAddress($this->to);
+        $this->PHPMailer->isHTML();
+        $this->PHPMailer->CharSet = 'UTF-8';
+        $this->PHPMailer->Body = file_get_contents($this->htmlPath);
+    }
+
+    public function send(): void
+    {
+        try {
+            $this->createMailerInstance();
+            $this->mountMail();
+
+            $this->PHPMailer->send();
+        } catch (Exception $e) {
+            // @TODO : Tratar erro de uma forma melhor
+            var_dump($e);
+        }
+    }
+}
