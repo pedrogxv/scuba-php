6f34be88a309ae012c5019ece543c9bc1b774159
Pedro Gabriel <63373521+PedroGXV@users.noreply.github.com>
2023-04-30 13:16:11 -0300

    Adicionado docblock para session.php

--- utils/session.php
+++ utils/session.php
@@ -1,10 +1,16 @@
 <?php
 
+/**
+ * Put a successful message in the session bag.
+ */
 function put_flash_message(string $message): void
 {
     $_SESSION[FLASH_MESSAGE_SESSION_NAME] = [ $message ];
 }
 
+/**
+ * Put an error message in the session bag.
+ */
 function put_error_message(string $message): void
 {
     $_SESSION[ERROR_MESSAGES_SESSION_NAME] = [ $message ];
